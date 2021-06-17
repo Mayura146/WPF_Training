4766a0d24bc330be07fb6347b323f0ed994c5572
MayuraEMI-146 <66059681+MayuraEMI-146@users.noreply.github.com>
2021-06-17 15:22:00 +0530

    Login_Registration Page

--- Registration_Login/Login_Page.xaml.cs
+++ Registration_Login/Login_Page.xaml.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -19,9 +22,59 @@ namespace Registration_Login
     /// </summary>
     public partial class Login_Page : Window
     {
+        Registration_Page registration = new Registration_Page();
+        Welcome welcome = new Welcome();
         public Login_Page()
         {
             InitializeComponent();
         }
+        // login
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            if (textBoxEmail.Text.Length == 0)
+            {
+                errormessage.Text = "Enter an email.";
+                textBoxEmail.Focus();
+            }
+            else if (!Regex.IsMatch(textBoxEmail.Text, @"^[a-zA-Z][\w\.-]*[a-zA-Z0-9]@[a-zA-Z0-9][\w\.-]*[a-zA-Z0-9]\.[a-zA-Z][a-zA-Z\.]*[a-zA-Z]$"))
+            {
+                errormessage.Text = "Enter a valid email.";
+                textBoxEmail.Select(0, textBoxEmail.Text.Length);
+                textBoxEmail.Focus();
+            }
+            else
+            {
+                string email = textBoxEmail.Text;
+                string password = passwordBox1.Password;
+                SqlConnection con = new SqlConnection("Data Source=UT-LTP-636;Initial Catalog=Training; Integrated Security=true");
+                con.Open();
+                SqlCommand cmd = new SqlCommand("Select * from Registartion where emailid='" + email + "'  and password='" + password + "'", con);
+                cmd.CommandType = CommandType.Text;
+                SqlDataAdapter adapter = new SqlDataAdapter();
+                adapter.SelectCommand = cmd;
+                DataSet dataSet = new DataSet();
+                adapter.Fill(dataSet);
+                if (dataSet.Tables[0].Rows.Count > 0)
+                {
+                    string username = dataSet.Tables[0].Rows[0]["fname"].ToString() + " " + dataSet.Tables[0].Rows[0]["lname"].ToString();
+                    welcome.TextBlockName.Text = username;//Sending value from one form to another form.  
+                    welcome.Show();
+                    Close();
+                }
+                else
+                {
+                    errormessage.Text = "Sorry! Please enter existing emailid/password.";
+                }
+                con.Close();
+            }
+        }
+
+        private void buttonRegister_Click(object sender, RoutedEventArgs e)
+        {
+            registration.Show();
+            Close();
+        }
+
     }
 }
+
